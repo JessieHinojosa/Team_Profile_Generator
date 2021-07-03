5f605fe48e0196c33f990354380257bc2891335c
webMavrick <waterpro92@yahoo.com>
2021-07-03 17:51:26 -0400

    added style and fixed comma bug

--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -1,43 +1,48 @@
-const fs = require('fs');
 const generateEngineerCard = (employeeData) => {
     return `
-    <div class="card col-1.5 p-1" style="width: 18rem;">
+    <div class="card col-1.5 p-1 shadow p-3 mb-5 bg-body rounded-3" style="width: 18rem;">
+     <div class="bg-info p-1 rounded-3">
       <h3 class="card-title my-1">${employeeData.name}</h3>
-      <h5 class="card-subtitle mb-2 text-muted">${employeeData.role}</h5>
+      <h5 class="card-subtitle mb-2">${employeeData.role}</h5>
+     </div>
       <ul class="pl-1 list-unstyled">
         <li>Id: ${employeeData.id}</li>
         <li>Email: ${employeeData.email}</li>
         <li>Github: <a href="https://github.com/${employeeData.github}/">${employeeData.github}</a></li>
         </ul>
-        </div>
+    </div>
         `
-    }
-    const generateInternCard = (employeeData) => {
-        return `
-        <div class="card col-1.5 p-1" style="width: 18rem;">
-        <h3 class="card-title my-1">${employeeData.name}</h3>
-        <h5 class="card-subtitle mb-2 text-muted">${employeeData.role}</h5>
+}
+const generateInternCard = (employeeData) => {
+    return `
+    <div class="card col-1.5 p-1 shadow p-3 mb-5 bg-body rounded-3" style="width: 18rem;">
+     <div class="bg-info p-1 rounded-3">
+      <h3 class="card-title my-1">${employeeData.name}</h3>
+      <h5 class="card-subtitle mb-2 ">${employeeData.role}</h5>
+     </div>
         <ul class="pl-1 list-unstyled">
         <li>Id: ${employeeData.id}</li>
         <li>Email: ${employeeData.email}</li>
         <li>School: ${employeeData.school}</li>
         </ul>
-        </div>
-        `
-    }
-    const generateManagaerCard = (employeeData) => {
-        return `
-        <div class="card col-1.5 p-1" style="width: 18rem;">
-        <h3 class="card-title my-1">${employeeData.name}</h3>
-        <h5 class="card-subtitle mb-2 text-muted">${employeeData.role}</h5>
-        <ul class="pl-1 list-unstyled">
-        <li>Id: ${employeeData.id}</li>
-        <li>Email: <a href="mailto:${employeeData.email}">${employeeData.email}</a></li>
-        <li>Office: ${employeeData.officeNumber}</li>
-        </ul>
-        </div>
-        `
-    }
+    </div>
+    `
+}
+const generateManagaerCard = (employeeData) => {
+    return `
+    <div class="card col-1.5 p-1 shadow p-3 mb-5 bg-body rounded-3" style="width: 18rem;">
+     <div class="bg-info p-1 rounded-3">
+    <h3 class="card-title my-1">${employeeData.name}</h3>
+    <h5 class="card-subtitle mb-2 ">${employeeData.role}</h5>
+     </div>
+    <ul class="pl-1 list-unstyled">
+    <li>Id: ${employeeData.id}</li>
+    <li>Email: <a href="mailto:${employeeData.email}">${employeeData.email}</a></li>
+    <li>Office: ${employeeData.officeNumber}</li>
+    </ul>
+    </div>
+    `
+}
 
 const generateHtml = (teamArray) => {
     const employeeCards = [];
@@ -54,8 +59,7 @@ const generateHtml = (teamArray) => {
         }
 
     });
-    console.log(employeeCards)
-    // create elements for each object
+
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -73,11 +77,7 @@ const generateHtml = (teamArray) => {
      <main class="mt-5">
      <section class="container-fluid">
       <div class="row justify-content-around">
-        ${employeeCards[0]}
-        ${employeeCards[1]}       
-        ${employeeCards[2]}       
-        ${employeeCards[3]} 
-      
+        ${employeeCards.join('')}
      </section>
      </main>
     </body>
